6791bc4da2510f0ccd80ddf3245b817f996e314c
miyaz <sinji_mg@yahoo.co.jp>
2021-05-22 13:49:43 +0900

    add ec2 userdata script

---
+++ userdata.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## ec2 userdata script
+
+# docker setup
+yum install -y docker
+usermod -a -G docker ec2-user
+systemctl enable docker
+systemctl start docker
+
+# gelbo auto start
+docker pull public.ecr.aws/h0g2h5b7/gelbo
+chmod +x /etc/rc.d/rc.local
+echo 'docker run -dit --restart always --name gelbo -p 80:9000 public.ecr.aws/h0g2h5b7/gelbo' >> /etc/rc.d/rc.local
+
+reboot
