da95fc41f025f0dca2c3f08f324bc45acd8cde1e
miyaz <sinji_mg@yahoo.co.jp>
2021-05-23 14:22:16 +0900

    docker pull new image at startup

--- userdata.sh
+++ userdata.sh
@@ -11,6 +11,7 @@ systemctl start docker
 # gelbo auto start
 docker pull public.ecr.aws/h0g2h5b7/gelbo
 chmod +x /etc/rc.d/rc.local
+echo 'docker pull public.ecr.aws/h0g2h5b7/gelbo' >> /etc/rc.d/rc.local
 echo 'docker run -dit --restart always --name gelbo -p 80:80 public.ecr.aws/h0g2h5b7/gelbo' >> /etc/rc.d/rc.local
 
 reboot
