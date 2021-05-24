c91908ebcfad7dd350eec1192a4b4dc6852b0a6a
miyaz <sinji_mg@yahoo.co.jp>
2021-05-24 18:52:42 +0900

    no need to reboot in userdata

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ## ec2 userdata script
 
@@ -8,10 +8,5 @@ usermod -a -G docker ec2-user
 systemctl enable docker
 systemctl start docker
 
-# gelbo auto start
-docker pull public.ecr.aws/h0g2h5b7/gelbo
-chmod +x /etc/rc.d/rc.local
-echo 'docker pull public.ecr.aws/h0g2h5b7/gelbo' >> /etc/rc.d/rc.local
-echo 'docker run -dit --restart always --name gelbo -p 80:80 public.ecr.aws/h0g2h5b7/gelbo' >> /etc/rc.d/rc.local
-
-reboot
+# gelbo start
+docker run -dit --restart always --name gelbo -p 80:80 public.ecr.aws/h0g2h5b7/gelbo
