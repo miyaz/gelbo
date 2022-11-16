d05497184eda5c6cf79160c55980382d6b7cc5e2
Shinji Miyazato <shinjimi@amazon.co.jp>
2022-11-16 11:59:24 +0900

    support for getting AZ on ECS

--- init.go
+++ init.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"io/ioutil"
 	"net/http"
@@ -27,14 +28,32 @@ func init() {
 		Int("timeout", idleTimeout).
 		Bool("nolog", noLog).Logger()
 
-	if az := getEC2MetaData("availability-zone"); az != "" {
+	if metaDataType := getMetaDataType(); metaDataType != "" {
 		zlog.Log().Msg("running on AWS")
 		runOnAws = true
-		store.host.AZ = az
-		store.host.InstanceType = getEC2MetaData("instance-type")
+		store.host.AZ = getAZ(metaDataType)
+		if metaDataType == "ec2" {
+			store.host.InstanceType = getEC2MetaData("instance-type")
+		}
 	} else {
 		zlog.Log().Msg("running on non-AWS")
 	}
+}
+
+func getMetaDataType() string {
+	client := http.Client{
+		Timeout: time.Second,
+	}
+	if az := getFromIMDS("/placement/availability-zone"); az != "" {
+		return "ec2"
+	} else if endpoint := os.Getenv("ECS_CONTAINER_METADATA_URI_V4"); endpoint != "" {
+		_, err := client.Get(endpoint)
+		if err != nil {
+			return ""
+		}
+		return "ecs"
+	}
+	return ""
 }
 
 func getFromIMDS(path string) (data string) {
@@ -75,4 +94,44 @@ func getEC2MetaData(field string) (value string) {
 		}
 	}
 	return
+}
+
+// for unmarshal from {ECS_CONTAINER_METADATA_URI_V4}/task
+type MetadataTask struct {
+	//Cluster          string
+	//TaskARN          string
+	AvailabilityZone string
+}
+
+func getContainerMetadata(field string) string {
+	// now not using field variable
+	// TODO: get a value specified by field variable
+
+	addr := os.Getenv("ECS_CONTAINER_METADATA_URI_V4") + "/task"
+	client := http.Client{
+		Timeout: time.Second,
+	}
+	resp, err := client.Get(addr)
+	if err != nil {
+		return ""
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	var md MetadataTask
+	if err := json.Unmarshal(body, &md); err != nil {
+		return ""
+	}
+
+	return string(md.AvailabilityZone)
+}
+
+func getAZ(metaDataType string) string {
+	if metaDataType == "ec2" {
+		return getEC2MetaData("availability-zone")
+	} else if metaDataType == "ecs" {
+		return getContainerMetadata("availability-zone")
+	}
+	return ""
 }
