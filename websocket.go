8f752feb232d4144e7091bf5af8912ac061e5802
miyaz <sinji_mg@yahoo.co.jp>
2024-05-17 18:35:47 +0900

    Feature: websocket #1

--- websocket.go
+++ websocket.go
@@ -11,10 +11,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -45,12 +47,18 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-// Message is struct(json) of WebSocket communication.
-type Message struct {
-	Method   string    `json:"method,omitempty"`
-	Message  string    `json:"message,omitempty"`
-	Time     time.Time `json:"time,omitempty"`
-	ClientId string    `json:"clientId,omitempty"`
+// WsData is struct(json) of WebSocket communication.
+type WsData struct {
+	Type     string `json:"type"`
+	Message  string `json:"message,omitempty"`
+	SendTime int64  `json:"sendTime,omitempty"`
+	User     User   `json:"user,omitempty"`
+}
+
+// User is part of UserList
+type User struct {
+	ClientId string `json:"clientId,omitempty"`
+	Color    string `json:"color,omitempty"`
 }
 
 // Client is a middleman between the websocket connection and the hub.
@@ -65,7 +73,6 @@ type Client struct {
 
 	// client identifier
 	id    string
-	name  string
 	color string
 
 	// closed flag
@@ -141,28 +148,34 @@ func chatPageHandler(w http.ResponseWriter, r *http.Request) {
 	httpLog(reqtime, int64(len(chatHtml)), http.StatusOK, r)
 }
 
-// wsHandler handles websocket requests from the peer.
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("upgrader.Upgrade error: %v", err)
-		return
-	}
-	fmt.Printf("openWS RemoteAddr: %v, X-Forwarded-For: %v\n", r.RemoteAddr, r.Header.Get("X-Forwarded-For"))
-
+func wsLogger(r *http.Request) *zerolog.Logger {
 	proto, _ := r.Context().Value("proto").(string)
 	remotePort := extractPort(r.RemoteAddr)
 	remoteAddr := extractIPAddress(r.RemoteAddr)
 	logger := zerolog.New(os.Stdout).With().
 		Time("conntime", time.Now()).
 		Str("proto", proto).
+		Str("clientip", getClientIPAddress(r)).
 		Str("srcip", remoteAddr).
 		Int("srcport", remotePort).
 		Logger()
+	return &logger
+}
 
-	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256), id: getClientID(r, conn), name: "John Doe", color: "green"}
+// wsHandler handles websocket requests from the peer.
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	logger := wsLogger(r)
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		logger.Log().Str("error", fmt.Sprintf("upgrader.Upgrade error: %v", err)).Msg("")
+		return
+	}
+
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256), id: getClientID(r, conn), color: getRandomColor()}
 	client.hub.register <- client
+
+	logger.Log().Str("color", client.color).Msg("connected")
 
 	// Allow collection of memory referenced by the caller by doing all work in
 	// new goroutines.
@@ -178,12 +191,56 @@ func getClientID(r *http.Request, conn *websocket.Conn) (clientID string) {
 	return strings.Replace(clientID, " ", "", -1)
 }
 
+var defColors = []string{"aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "silver", "teal", "white", "yellow"}
+
+// Do not duplicate color selections until all color definitions are used
+func getRandomColor() string {
+	var usedColors []string
+	for c := range hub.clients {
+		if !slices.Contains(usedColors, c.color) {
+			usedColors = append(usedColors, c.color)
+		}
+	}
+	diffColors := diffSlice(defColors, usedColors)
+	if len(diffColors) == 0 {
+		randIdx := rand.Intn(len(defColors))
+		return defColors[randIdx]
+	} else {
+		randIdx := rand.Intn(len(diffColors))
+		return diffColors[randIdx]
+	}
+}
+
+// subtraction between arrays
+func diffSlice[T comparable](slice1, slice2 []T) []T {
+	diffSlice := []T{}
+	cmpMap := map[T]int{}
+
+	for _, v := range slice2 {
+		cmpMap[v] += 1
+	}
+
+	for _, v := range slice1 {
+		t, ok := cmpMap[v]
+		if !ok {
+			diffSlice = append(diffSlice, v)
+			continue
+		}
+		if t == 1 {
+			delete(cmpMap, v)
+		} else {
+			cmpMap[v] -= 1
+		}
+	}
+	return diffSlice
+}
+
 // readPump pumps messages from the websocket connection to the hub.
 //
 // The application runs readPump in a per-connection goroutine. The application
 // ensures that there is at most one reader on a connection by executing all
 // reads from this goroutine.
-func (c *Client) readPump(logger zerolog.Logger) {
+func (c *Client) readPump(logger *zerolog.Logger) {
 	defer func() {
 		c.hub.unregister <- c
 		c.conn.Close()
@@ -191,28 +248,77 @@ func (c *Client) readPump(logger zerolog.Logger) {
 	c.conn.SetReadLimit(maxMessageSize)
 	c.conn.SetReadDeadline(time.Now().Add(pongWait))
 	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	var wsInData WsData
+	var wsOutData WsData
 	for {
 		_, message, err := c.conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("UnexpectedCloseError error: %v", err)
+				logger.Log().Time("closetime", time.Now()).
+					Str("error", fmt.Sprintf("UnexpectedCloseError: %v", err)).Msg("")
 			} else {
-				log.Printf("ExpectedCloseError error: %v", err)
+				logger.Log().Time("closetime", time.Now()).
+					Str("error", fmt.Sprintf("ExpectedCloseError: %v", err)).Msg("")
 			}
 			// notification connection closed
-			c.closed = true
-			c.hub.broadcast <- []byte(fmt.Sprintf("Closed Connection with peer[%s] caused by [%v]", c.id, err))
-			break
-		}
-		logger2 := logger.With().
-			Time("readtime", time.Now()).Logger()
-		logger2.Log().Msg("pong")
+			wsOutData.Type = "deliverMessage"
+			wsOutData.Message = fmt.Sprintf("Disconnected due to [%v]", err)
+			wsOutData.SendTime = time.Now().UTC().UnixNano() / int64(time.Millisecond)
+			wsOutData.User.ClientId = c.id
+			wsOutData.User.Color = c.color
 
-		message = []byte(fmt.Sprintf(`{"message":"%s", "id":"%s", "name":"%s", "color":"%s"}`, message, c.id, c.name, c.color))
-		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
-		c.hub.broadcast <- message
-		log.Printf("msg from: %s[%s], message: %s", c.name, c.id, message)
+			message = convertWsData2JSON(wsOutData)
+			c.closed = true
+			c.hub.broadcast <- message
+			break
+		} else {
+			logger.Log().Time("readtime", time.Now()).Int("msgsize", len(string(message))).Msg("")
+		}
+
+		if err := json.Unmarshal(message, &wsInData); err != nil {
+			c.send <- message
+		} else {
+			if wsInData.Type == "whoAmI" {
+				// reply connection info to the user
+				wsOutData.Type = "yourInfo"
+				wsOutData.User.ClientId = c.id
+				wsOutData.User.Color = c.color
+				message = convertWsData2JSON(wsOutData)
+				c.send <- message
+
+				// send connection opened message to all users
+				wsOutData.Type = "deliverMessage"
+				wsOutData.Message = "Connection opened."
+				wsOutData.SendTime = time.Now().UTC().UnixNano() / int64(time.Millisecond)
+				wsOutData.User.ClientId = c.id
+				wsOutData.User.Color = c.color
+				message = convertWsData2JSON(wsOutData)
+				c.hub.broadcast <- message
+			} else {
+				wsOutData.Message = wsInData.Message
+				wsOutData.SendTime = time.Now().UTC().UnixNano() / int64(time.Millisecond)
+				wsOutData.User.ClientId = c.id
+				wsOutData.User.Color = c.color
+				if wsInData.Type == "postToChat" {
+					// send chat message to all users
+					wsOutData.Type = "deliverMessage"
+					message = convertWsData2JSON(wsOutData)
+					c.hub.broadcast <- message
+				} else if wsInData.Type == "echoMessage" {
+					// send echo message to the user
+					wsOutData.Type = "echoReply"
+					message = convertWsData2JSON(wsOutData)
+					c.send <- message
+				}
+			}
+		}
 	}
+}
+
+func convertWsData2JSON(wsData WsData) []byte {
+	wsDataJSON, _ := json.Marshal(wsData)
+	respJSON := bytes.TrimSpace(bytes.Replace(wsDataJSON, newline, space, -1))
+	return respJSON
 }
 
 // writePump pumps messages from the hub to the websocket connection.
@@ -220,7 +326,7 @@ func (c *Client) readPump(logger zerolog.Logger) {
 // A goroutine running writePump is started for each connection. The
 // application ensures that there is at most one writer to a connection by
 // executing all writes from this goroutine.
-func (c *Client) writePump(logger zerolog.Logger) {
+func (c *Client) writePump(logger *zerolog.Logger) {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
 		ticker.Stop()
@@ -229,10 +335,8 @@ func (c *Client) writePump(logger zerolog.Logger) {
 	for {
 		select {
 		case message, ok := <-c.send:
-			log.Printf("msg   to: %s, message: %s", c.id, message)
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
-				log.Println("get message from c.send not ok")
 				// The hub closed the channel.
 				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
@@ -240,10 +344,11 @@ func (c *Client) writePump(logger zerolog.Logger) {
 
 			w, err := c.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
-				log.Printf("c.send triggered c.conn.NextWriter error: %v", err)
+				logger.Log().Time("closetime", time.Now()).
+					Str("error", fmt.Sprintf("conn.NextWriter error: %v", err)).Msg("")
 				// notification connection closed
 				c.closed = true
-				c.hub.broadcast <- []byte(fmt.Sprintf("Closed Connection with peer[%s] caused by [%v] in NextWriter", c.id, err))
+				c.hub.broadcast <- []byte(fmt.Sprintf("Disconnected due to [%v] in NextWriter", err))
 				return
 			}
 			w.Write(message)
@@ -256,26 +361,28 @@ func (c *Client) writePump(logger zerolog.Logger) {
 			}
 
 			if err := w.Close(); err != nil {
-				log.Printf("c.send triggered w.Close error: %v", err)
+				logger.Log().Time("closetime", time.Now()).
+					Str("error", fmt.Sprintf("conn.NextWriter.Close error: %v", err)).Msg("")
 				// notification connection closed
 				c.closed = true
-				c.hub.broadcast <- []byte(fmt.Sprintf("Closed Connection with peer[%s] caused by [%v] in w.Close", c.id, err))
+				c.hub.broadcast <- []byte(fmt.Sprintf("Disconnected due to [%v] in sNextWriter.Close", err))
 				return
 			}
-		case <-ticker.C:
-			logger2 := logger.With().
-				Time("writetime", time.Now()).Logger()
-			logger2.Log().Msg("ping")
+			logger.Log().Time("writetime", time.Now()).Int("msgsize", len(string(message))).Msg("")
 
+		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			log.Printf("ping  to: %s", c.id)
+			//log.Printf("ping  to: %s", c.id)
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				log.Printf("ticker triggered c.conn.WriteMessage error: %v", err)
+				logger.Log().Time("closetime", time.Now()).
+					Str("error", fmt.Sprintf("c.conn.WriteMessage error: %v", err)).Msg("")
 				// notification connection closed
 				c.closed = true
-				c.hub.broadcast <- []byte(fmt.Sprintf("Closed Connection with peer[%s] caused by [%v] in WriteMessage", c.id, err))
+				c.hub.broadcast <- []byte(fmt.Sprintf("Disconnected due to [%v] in WriteMessage", err))
 				return
 			}
+			logger.Log().Time("pingtime", time.Now()).Msg("")
+
 		}
 	}
 }
